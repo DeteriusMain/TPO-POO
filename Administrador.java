2cff42ca9ab126ca71ba54aae7ce7af579d68b97
Ezequiel Bernardo <ezbernardo@uade.edu.ar>
2025-06-02 10:35:55 -0300

    Actualizacion parcial

--- Administrador.java
+++ Administrador.java
@@ -1,14 +1,16 @@
-public class Administrador{
+public class Administrador {
+    // Atributo de la clase Administrador
     public int dinero;
 
+    // Constructor
     public Administrador(int dinero) {
         this.dinero = dinero;
     }
 
+    // Getters and Setters
     public int getDinero() {
         return dinero;
     }
-
     public void setDinero(int dinero) {
         this.dinero = dinero;
     }
