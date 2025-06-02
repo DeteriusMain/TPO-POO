ba1481cab5ac6ce34af26bdc81b636a96094abfb
Ivan <ivanfaifer@hotmail.com.ar>
2025-06-02 10:25:27 -0300

    Clase hecha potr la mitad preguntar al profesor por los metodos de esta clase

---
+++ Administrador.java
@@ -0,0 +1,17 @@
+public class Administrador{
+    public int dinero;
+
+    public Administrador(int dinero) {
+        this.dinero = dinero;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+
+
+}
